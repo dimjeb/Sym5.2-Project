8df7717a15583d55514f3f3147704a7569206cf3
Dmitriy Zhebrovskiy <dimjeb@gmail.com>
2022-02-11 10:13:36 +0800

    CLI util for add users/admins

--- src/Command/AddUserCommand.php
+++ src/Command/AddUserCommand.php
@@ -2,6 +2,11 @@
 
 namespace App\Command;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Exception\RuntimeException;
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -9,12 +14,24 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
 
 class AddUserCommand extends Command
 {
     protected static $defaultName = 'app:add-user';
     protected static $defaultDescription = 'Create user';
+    private EntityManagerInterface $entityManager;
+    private UserPasswordEncoderInterface $encoder;
+    private UserRepository $userRepository;
+
+    public function __construct(string $name = null, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder, UserRepository $userRepository)
+    {
+        parent::__construct($name);
+        $this->entityManager = $entityManager;
+        $this->encoder = $encoder;
+        $this->userRepository = $userRepository;
+    }
 
     protected function configure(): void
     {
@@ -52,6 +69,18 @@ class AddUserCommand extends Command
             $question = new Question('Is admin? (1 or 0)', 0);
             $isAdmin = $io->askQuestion($question);
         }
+
+        $isAdmin = boolval($isAdmin);
+
+        try {
+            $user = $this->createUser($email, $password, $isAdmin);
+        } catch (RuntimeException $exception) {
+            $io->comment($exception->getMessage());
+
+            return Command::FAILURE;
+        }
+
+
         $successMessage = sprintf('%s was successfully created: %s',
             $isAdmin ? 'Administrator user' : 'User',
             $email
@@ -59,7 +88,7 @@ class AddUserCommand extends Command
         $io->success($successMessage);
         $event = $stopwatch->stop('add-user-command');
         $stopwatchMessage = sprintf('New user\'s id: %d / Elapsed time: %.2f ms / Consumed memory: %.2f MB',
-            123,
+            $user->getId(),
             $event->getDuration(),
             $event->getMemory() / 1000 / 1000
         );
@@ -77,5 +106,32 @@ class AddUserCommand extends Command
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 */
         return Command::SUCCESS;
+    }
+
+    /**
+     * @param string $email
+     * @param string $password
+     * @param Boolean $isAdmin
+     * @return User
+     */
+    private function createUser(string $email, string $password, $isAdmin): User
+    {
+        $existingUser = $this->userRepository->findOneBy(['email' => $email]);
+
+        if ($existingUser) {
+            throw new RuntimeException('User already exist');
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles([$isAdmin ? 'ROLE_ADMIN' : 'ROLE_USER']);
+
+        $encodedPassword = $this->encoder->encodePassword($user, $password);
+        $user->setPassword($encodedPassword);
+        $user->setIsVerified(true);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
     }
 }
